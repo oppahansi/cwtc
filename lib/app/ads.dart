fd9b37f3e4670d1752f3ad60efeca5785a97686c
Alexander Schellenberg <oppahansi@gmail.com>
2021-10-17 11:11:16 +0200

    Test Git alliases

--- lib/app/ads.dart
+++ lib/app/ads.dart
@@ -1,16 +1,6 @@
 import 'dart:io';
 
 class AdManager {
-  static String get appId {
-    if (Platform.isAndroid) {
-      return "ca-app-pub-3940256099942544/5662855259";
-    } else if (Platform.isIOS) {
-      return "ca-app-pub-3940256099942544/5662855259";
-    } else {
-      throw UnsupportedError("Unsupported platform");
-    }
-  }
-
   static String get bannerAdUnitId {
     if (Platform.isAndroid) {
       return "ca-app-pub-3940256099942544/6300978111";
@@ -20,24 +10,4 @@ class AdManager {
       throw UnsupportedError("Unsupported platform");
     }
   }
-
-  // static String get interstitialAdUnitId {
-  //   if (Platform.isAndroid) {
-  //     return "ca-app-pub-3940256099942544/2247696110";
-  //   } else if (Platform.isIOS) {
-  //     return "<YOUR_IOS_INTERSTITIAL_AD_UNIT_ID>";
-  //   } else {
-  //     throw new UnsupportedError("Unsupported platform");
-  //   }
-  // }
-
-  // static String get rewardedAdUnitId {
-  //   if (Platform.isAndroid) {
-  //     return "<YOUR_ANDROID_REWARDED_AD_UNIT_ID>";
-  //   } else if (Platform.isIOS) {
-  //     return "<YOUR_IOS_REWARDED_AD_UNIT_ID>";
-  //   } else {
-  //     throw new UnsupportedError("Unsupported platform");
-  //   }
-  // }
 }
